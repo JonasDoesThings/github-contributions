6ce24277a3753382e581918cae056e9bdf63a913
JonasDoesThings <jonas@jonasdoesthings.com>
2024-07-03 20:35:28 +0200

    fix: fix scraping logic

--- src/utils/contributions_parser.ts
+++ src/utils/contributions_parser.ts
@@ -10,11 +10,15 @@ const CONTRIBUTIONS_COUNT_REGEX = /(?<contributionscount>No|\d+) contribution(s?
 function parseGitHubContributionsHTML(rawHTML: string) {
   const contributions: {[key: string]: number} = {};
 
+  // example html format as of 2024-07-03 for a single day entry:
+  // <td tabindex="0" data-ix="1" aria-selected="false" aria-describedby="contribution-graph-legend-level-2" style="width: 10px" data-date="2022-01-03" id="contribution-day-component-1-1" data-level="2" role="gridcell" data-view-component="true" class="ContributionCalendar-day"></td>
+  // <tool-tip id="tooltip-654a84c9-50b4-4058-a30a-6a606d396b3d" for="contribution-day-component-1-1" popover="manual" data-direction="n" data-type="label" data-view-component="true" class="sr-only position-absolute">6 contributions on January 3rd.</tool-tip>
   const htmlLines = rawHTML.split('\n');
   htmlLines
-    .filter(line => line.trimStart().startsWith('<td'))
-    .forEach(line => {
-      const date = ISO_DATE_REGEX.exec(line)?.groups!['date'];
+    .forEach((line, lineIndex) => {
+      if(!line.trimStart().startsWith('<tool-tip')) return;
+
+      const date = ISO_DATE_REGEX.exec(htmlLines[lineIndex-1])?.groups!['date'];
       if(!date) {
         return;
       }
