cfe1fe6f221ae3b9784f2083a9da36d5f025cab4
JonasDoesThings <jonas@jonasdoesthings.com>
2026-04-12 19:55:51 +0200

    build: migrate eslint from 8 to 10

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'single'],
+      'jsx-quotes': ['error', 'prefer-single'],
+      'semi': ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@stylistic/member-delimiter-style': 'error',
+    },
+  },
+);
